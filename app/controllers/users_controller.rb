9a54f4a52420dccd575b5cbbd12455c04d27ef68
shoheiyamaguchi <tohinata.dev@gmail.com>
2016-07-03 04:33:30 +0000

    login-logout create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-      
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
   
